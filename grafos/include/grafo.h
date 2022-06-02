cc07d44a2108432d71c4cd1d91486f5c70a7466b
fabiokleis <fabiohkrc@gmail.com>
2022-06-02 00:57:36 -0300

    resolvido grafos com peso e direcionamento, lista adjacente com direcionamento e matriz adjacente com direcionamento, criado algumas funcoes para mostrar vizinhos, numero de arestas que saem e arestas incidentes etc...

--- grafos/include/grafo.h
+++ grafos/include/grafo.h
@@ -8,10 +8,23 @@ typedef struct noA {
 
 /*Estrutura de Grafo com lista de adjacências: */
 typedef struct grafoA {
-   int E; /* Quantidade de arestas. */
-   int V; /* Quantidade de vértices. */
-   NoA **Adj; /* Lista de adjacências. */
+    int E; /* Quantidade de arestas. */
+    int V; /* Quantidade de vértices. */
+    NoA **Adj; /* Lista de adjacências. */
 } GrafoA;
+
+typedef struct noA_p {
+    int id;          /*Identificador armazenado no nó. */
+    int payload;     /* Peso armazenado no nó */
+    struct noA_p *next; /* Próximo nó na lista encadeada. */
+} NoA_p;
+
+/*Estrutura de Grafo com lista de adjacências com peso */
+typedef struct grafoAP {
+    int E; /* Quantidade de arestas. */
+    int V; /* Quantidade de vértices. */
+    NoA_p **Adj; /* Lista de adjacências. */
+} GrafoAP;
 
 /*Estrutura de Grafo com matriz de adjacências: */
 typedef struct grafoM {
@@ -22,13 +35,25 @@ typedef struct grafoM {
 
 GrafoA* criar_grafo_adj (int tamanho);
 GrafoM* criar_grafo_mat (int tamanho);
+GrafoAP* criar_grafo_peso(int tamanho);
+NoA* inserir_no(NoA* no_a, int id);
+NoA_p* inserir_no_peso(NoA_p* no_a, int id, int peso);
 void liberar_grafo_adj (GrafoA *G);
 void liberar_grafo_mat (GrafoM *G);
+void liberar_grafo_adj_peso (GrafoAP *G);
 void print_grafoM(GrafoM *G);
 void print_grafoA(GrafoA *G);
+void print_arestaAP(GrafoAP *G);
 void inicia_matriz(GrafoM *G);
 void insere_arestaM(GrafoM *G, int v1, int v2);
-NoA* inserir_no(NoA* no_a, int id);
 void insere_arestaA(GrafoA *G, int v, int id);
+void insere_arestaM_peso(GrafoM *G, int v1, int v2, int peso);
+void insere_arestaA_peso(GrafoAP *G, int v, int id, int peso);
 
+void liste_a_incidentes_grafo_mat(GrafoM *G, int v);
+void liste_a_incidentes_grafo_adj(GrafoAP *G, int v);
+void lista_a_saem_grafo_mat(GrafoM *G, int v);
+void lista_a_saem_grafo_adj(GrafoAP *G, int v);
+void lista_v_visinhos_grafo_mat(GrafoM *G, int v);
+void lista_v_visinhos_grafo_adj(GrafoAP *G, int v);
 #endif /* end of include guard: GRAFO_H_4WSA51EH */
--- grafos/src/grafo.c
+++ grafos/src/grafo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "limits.h"
 #include "../include/grafo.h"
 
 /*Função para criar um grafo com lista de adjacências.*/
@@ -13,6 +14,18 @@ GrafoA* criar_grafo_adj (int tamanho) {
       G->Adj[v] = NULL;
    }
    return G;
+}
+
+GrafoAP* criar_grafo_adj_peso (int tamanho) {
+    int v;
+    GrafoAP *G = (GrafoAP *)malloc(sizeof(GrafoAP));
+    G->E = 0;
+    G->V = tamanho;
+    G->Adj = (NoA_p **)malloc(tamanho * sizeof(NoA_p *));
+    for (v = 0; v < G->V; v++) {
+        G->Adj[v] = NULL;
+    }
+    return G;
 }
 
 /*Função para criar um grafo com matriz de adjacências.*/
@@ -51,6 +64,17 @@ void liberar_grafo_mat (GrafoM *G) {
    free(G->Mat);
    free(G);
 }
+
+void liberar_grafo_adj_peso (GrafoAP *G) {
+    int v;
+    for (v = 0; v < G->V; v++) {
+        if (G->Adj[v] != NULL) {
+            free(G->Adj[v]);
+        }
+    }
+    free(G->Adj);
+    free(G);
+}
 // mostra o grafo M booleano;
 void print_grafoM (GrafoM *G) {
     printf("    ");
@@ -60,12 +84,13 @@ void print_grafoM (GrafoM *G) {
     printf("\n\n");
     for (int i = 0; i < G->V; i++) {
       printf("%d  ", i);
-      for (int j = 0; j < G->V; j++)
-         if (G->Mat[i][j] == 1) {
-            printf(" 1");
-         } else {
-            printf(" 0");
-         }
+      for (int j = 0; j < G->V; j++) {
+          if (G->Mat[i][j] < 0) {
+              printf(" 0");
+          } else {
+              printf(" %d", G->Mat[i][j]);
+          }
+      }
       printf( "\n");
     }
 }
@@ -81,19 +106,28 @@ void print_grafoA (GrafoA *G) {
         printf("\n");
     }
 }
-
-// coloca 0 em todas as posicoes da matriz;
+void print_arestaAP(GrafoAP *G) {
+    NoA_p *no_a = NULL;
+    for (int i = 0; i < G->V; i++) {
+        printf("%d -> ", i);
+        for (no_a = G->Adj[i]; no_a != NULL; no_a = no_a->next) {
+            printf(" %d (w:%d)", no_a->id, no_a->payload);
+        }
+        printf("\n");
+    }
+}
+// coloca INT_MIN em todas as posicoes da matriz;
 void inicia_matriz(GrafoM *G) {
     for (int i = 0; i < G->V; i++) {
         for (int j = 0; j < G->V; j++) {
-            G->Mat[i][j] = 0;
+            G->Mat[i][j] = INT_MIN;
         }
     }
 }
 
 // coloca 1 para a ligacao entre dois vertices
 void insere_arestaM(GrafoM *G, int v1, int v2) {
-    if (G->Mat[v1][v2] == 0) {
+    if (G->Mat[v1][v2] < 0) {
         G->Mat[v1][v2] = 1;
         G->E++; // incrementa o número total de arestas a cada insercao
     }
@@ -112,12 +146,97 @@ void insere_arestaA(GrafoA *G, int v, int id) {
     G->E++; // incrementa o numero total de arestas a cada insercao
 }
 
+void insere_arestaM_peso(GrafoM *G, int v1, int v2, int peso) {
+    if (G->Mat[v1][v2] < 0) {
+        G->Mat[v1][v2] = peso;
+        G->E++;
+    }
+}
 
+NoA_p* inserir_no_peso(NoA_p* no_a, int id, int peso) {
+    NoA_p *novo_a = (NoA_p *) malloc(sizeof(NoA_p));
+    novo_a->id = id;
+    novo_a->payload = peso;
+    novo_a->next = no_a; // insere na cabeça
+    return novo_a;
+}
+
+void insere_arestaA_peso(GrafoAP *G, int v, int id, int peso) {
+    G->Adj[v] = inserir_no_peso(G->Adj[v], id, peso);
+    G->E++; // incrementa o numero total de arestas a cada insercao
+}
+
+void liste_a_incidentes_grafo_mat(GrafoM *G, int v) {
+    int c = 0;
+    for (int i = 0; i < G->V; i++) {
+        if (G->Mat[v][i] > 0 || G->Mat[i][v] > 0) {
+            if (G->Mat[v][i] == v || G->Mat[i][v] == v) // remove a incidencia com ele `mesmo`?
+                continue;
+            c++;
+        }
+    }
+    printf("arestas incidentes em %d: %d\n", v, c);
+}
+// conta todos as arestas de um grafo e mostra
+void liste_a_incidentes_grafo_adj(GrafoAP *G, int v) {
+    int c = 0;
+    NoA_p *no_a = NULL;
+    for (int i = 0; i < G->V; i++) {
+        for (no_a = G->Adj[i]; no_a != NULL; no_a = no_a->next) {
+            if (no_a->id == v || (no_a->id != v && G->Adj[i] == G->Adj[v])) {
+                if (no_a->id == v && G->Adj[i] == G->Adj[v]) // remove a incidencia com ele `mesmo`?
+                    continue;
+                c++;
+            }
+        }
+    }
+    printf("arestas incidentes em %d: %d\n", v, c);
+}
+// conta todas as arestas que saem de um grafo e mostra
+void lista_a_saem_grafo_mat(GrafoM *G, int v) {
+    int c = 0;
+    for (int i = 0; i < G->V; i++) {
+        if (G->Mat[v][i] > 0) {
+            c++;
+        }
+    }
+    printf("arestas que saem %d: %d\n", v, c);
+}
+// conta todas as arestas que saem de um grafo e mostra
+void lista_a_saem_grafo_adj(GrafoAP *G, int v) {
+    int c = 0;
+    NoA_p *no_a = NULL;
+    for (no_a = G->Adj[v]; no_a != NULL; no_a = no_a->next) {
+        c++;
+    }
+    printf("arestas que saem %d: %d\n", v, c);
+}
+// mostra os vertices adjacentes a um determinado vertice
+void lista_v_visinhos_grafo_mat(GrafoM *G, int v) {
+    printf("vertecies adjacentes ao vertice %d:", v);
+    for (int i = 0; i < G->V; i++) {
+        if (G->Mat[v][i] > 0) {
+            if (G->Mat[v][i] == v) // remove a incidencia com ele `mesmo`?
+                continue;
+            printf(" %d", i);
+        }
+
+    }
+    printf("\n");
+}
+// mostra os vertices adjacentes a um determinado vertice
+void lista_v_visinhos_grafo_adj(GrafoAP *G, int v) {
+    printf("vertecies adjacentes ao vertice %d:", v);
+    NoA_p *no_a = NULL;
+    for (no_a = G->Adj[v]; no_a != NULL; no_a = no_a->next) {
+        printf(" %d", no_a->id);
+    }
+    printf("\n");
+}
 /* */
 int main () {
     // 1)
     /*
-     *
      * 0 -- 5
      * | \    \
      * |  2 -- 4
@@ -193,7 +312,7 @@ int main () {
 
     // 2)
     /*
-     *
+     * V    V     P
      * 0 --> 5
      * 0 --> 1
      * 1 --> 2
@@ -248,6 +367,81 @@ int main () {
 
     print_grafoA(Gad);
     liberar_grafo_adj(Gad);
+
+    // 3)
+    /*
+    • Liste o numero de arestas incidentes a um dado vertice.
+    • Liste o numero de arestas que saem de um dado vertice.
+    • Liste todas os vizinhos de um dado vertice (vertices adjacentes)
+ */
+    //Numero de arestas incidentes ao vertice 3: 4.
+    //Numero de arestas que saem do vertice 4: 2.
+    //Vertices adjacentes ao v́ertice 2: 0 3.
+    /*  V    V     P
+     * 0 --> 1     1
+     * 0 --> 3     5
+     * 1 --> 3     8
+     * 2 --> 0     5
+     * 2 --> 3     4
+     * 3 --> 3     6
+     * 3 --> 4     3
+     * 4 --> 2     7
+     * 4 --> 5     9
+     * 5 --> 0     2
+    */
+    printf("matriz de adjacencia direcionada com peso\n");
+    int Vmdp = 6; //Número de vértices
+    GrafoM* Gmd_p = criar_grafo_mat (Vmdp);
+    inicia_matriz(Gmd_p);
+    // 0
+    insere_arestaM_peso(Gmd_p, 0, 1, 1);
+    insere_arestaM_peso(Gmd_p, 0, 3, 5);
+    // 1
+    insere_arestaM_peso(Gmd_p, 1, 3, 8);
+    // 2
+    insere_arestaM_peso(Gmd_p, 2, 0, 5);
+    insere_arestaM_peso(Gmd_p, 2, 3, 4);
+    // 3
+    insere_arestaM_peso(Gmd_p, 3, 3, 6);
+    insere_arestaM_peso(Gmd_p, 3, 4, 3);
+    // 4
+    insere_arestaM_peso(Gmd_p, 4, 2, 7);
+    insere_arestaM_peso(Gmd_p, 4, 5, 9);
+    // 5
+    insere_arestaM_peso(Gmd_p, 5, 0, 2);
+
+    print_grafoM(Gmd_p);
+    liste_a_incidentes_grafo_mat(Gmd_p, 3);
+    lista_a_saem_grafo_mat(Gmd_p, 4);
+    lista_v_visinhos_grafo_mat(Gmd_p, 2);
+    liberar_grafo_mat (Gmd_p);
+
+    printf("lista de adjacencia direcionada com peso\n");
+    int Vad_p = 6; //Número de vértices
+    GrafoAP* Gad_p = criar_grafo_adj_peso(Vad_p);
+    // 0
+    insere_arestaA_peso(Gad_p, 0, 1, 1);
+    insere_arestaA_peso(Gad_p, 0, 3, 5);
+    // 1
+    insere_arestaA_peso(Gad_p, 1, 3, 8);
+    // 2
+    insere_arestaA_peso(Gad_p, 2, 0, 5);
+    insere_arestaA_peso(Gad_p, 2, 3, 4);
+    // 3
+    insere_arestaA_peso(Gad_p, 3, 3, 6);
+    insere_arestaA_peso(Gad_p, 3, 4, 3);
+    // 4
+    insere_arestaA_peso(Gad_p, 4, 2, 7);
+    insere_arestaA_peso(Gad_p, 4, 5, 9);
+    // 5
+    insere_arestaA_peso(Gad_p, 5, 0, 2);
+
+    print_arestaAP(Gad_p);
+    liste_a_incidentes_grafo_adj(Gad_p, 3);
+    lista_a_saem_grafo_adj(Gad_p, 4);
+    lista_v_visinhos_grafo_adj(Gad_p, 2);
+    liberar_grafo_adj_peso(Gad_p);
+
 
     return 0;
 }
